caf9e4a2bdcc1a19cb68199d276f9bf7909fadaa
unknown <rajasekhartechpy@gmail.com>
2020-02-27 14:31:42 +0530

    27chnage

--- task/settings.py
+++ task/settings.py
@@ -49,8 +49,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
+
 ]
 
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 ROOT_URLCONF = 'task.urls'
 
 TEMPLATES = [
